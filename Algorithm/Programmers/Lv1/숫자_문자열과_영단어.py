26ac5982adf57fed2248003db2c3ae71734abb97
yuuDev01 <pasenoaz@gmail.com>
2023-05-25 16:19:40 +0900

    시간복잡도 수정

--- Algorithm/Programmers/Lv1/숫자_문자열과_영단어.py
+++ Algorithm/Programmers/Lv1/숫자_문자열과_영단어.py
@@ -3,7 +3,22 @@
 numD = {'zero':'0','one':'1','two':'2','three':'3', 'four':'4','five':'5','six':'6', 'seven':'7', 'eight':'8','nine':'9'}
 
 def solution(s):
-    answer = 0
-    for k, v in numD.items():
-        s= s.replace(k,v)
-    return int(s)
+    answer = ''
+    n =''
+    for i in s :
+        if i.isdigit() :
+            answer +=i
+        else :
+            n+=i
+            if n in numD.keys():
+                answer += numD[n]
+                n = ''
+    return int(answer)
+
+
+# replace 사용 - 시간 복잡도 고려X
+# def solution(s):
+#     answer = 0
+#     for k, v in numD.items():
+#         s= s.replace(k,v)
+#     return int(s)
