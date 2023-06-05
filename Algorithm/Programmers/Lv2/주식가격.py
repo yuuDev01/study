a4ae2d7c46cc808f01014c9136abad2598c8a682
yuuDev01 <pasenoaz@gmail.com>
2023-06-05 23:48:36 +0900

    230605

--- Algorithm/Programmers/Lv2/주식가격.py
+++ Algorithm/Programmers/Lv2/주식가격.py
@@ -1,4 +1,18 @@
 # 출처: 프로그래머스 코딩 테스트 연습, https://school.programmers.co.kr/learn/courses/30/lessons/42584
 
-test=[1,2,3,4]
-print(test[0])
+from collections import deque 
+def solution(prices):
+    prices = deque(prices)
+    n = 0
+    answer = [0]*len(prices)
+    while prices:
+        price = prices.popleft()
+        if not prices : break
+        for i in prices:
+            if i >= price :
+                answer[n] += 1
+            else : 
+                answer[n]+=1
+                break
+        n+=1 
+    return answer
